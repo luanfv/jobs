913915f3742d46278180575be83209571d32886d
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-11-16 12:41:28 -0300

    Set information

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {MainStyle, PaperStyle, FormStyle,TitleStyle , TextFieldStyle, ButtonStyle, SpaceStyle} from './../../theme/styled'
 
-export default () => (
-    <MainStyle>
-        <PaperStyle>
-            <FormStyle>
-                <TitleStyle fontSize='24'>Criar usuário</TitleStyle>
-                <SpaceStyle padding='10' />
-                
-                <SpaceStyle padding='5' />
-                <TextFieldStyle label='Nome' />
-                
-                <SpaceStyle padding='5' />
-                <TextFieldStyle label='Vaga' />
-                
-                <SpaceStyle padding='5' />
-                <TextFieldStyle label='Data de Nascimento' />
+export default () => {
+    const [name, setName] = useState('')
+    const [stack, setStack] = useState('')
+    const [birth, setBrith] = useState('')
+    const [email, setEmail] = useState('')
 
-                <TextFieldStyle label='Email' />
+    const handleUser = () => {
+        const user = {name: name, stack: stack, birth: birth, email: email}
+        console.log(user)
+    }
 
-                <SpaceStyle padding='10' />
-                <ButtonStyle variant="contained" color="primary" width="280">Enviar</ButtonStyle>
-            </FormStyle>
-        </PaperStyle>
-    </MainStyle>
-)
+    return (
+        <MainStyle>
+            <PaperStyle>
+                <FormStyle>
+                    <TitleStyle fontSize='24'>Criar usuário</TitleStyle>
+                    <SpaceStyle />
+                    
+                    <SpaceStyle padding='5' />
+                    <TextFieldStyle label='Nome' value={name} onChange={(e) => setName(e.target.value)} value={name} />
+                    
+                    <SpaceStyle padding='5' />
+                    <TextFieldStyle label='Vaga' value={stack} onChange={(e) => setStack(e.target.value)} value={stack} />
+                    
+                    <SpaceStyle padding='5' />
+                    <TextFieldStyle label='Data de Nascimento' value={birth} onChange={(e) => setBrith(e.target.value)} value={birth} />
+
+                    <TextFieldStyle label='Email' value={email} onChange={(e) => setEmail(e.target.value)} value={email} />
+
+                    <SpaceStyle />
+                    <ButtonStyle variant="contained" color="primary" width="280" onClick={handleUser}>Enviar</ButtonStyle>
+                </FormStyle>
+            </PaperStyle>
+        </MainStyle>
+    )
+}
