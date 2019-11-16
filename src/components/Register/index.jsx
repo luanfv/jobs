c030ab4f2ebba3b33f20221ef42c7ec28156b6f8
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-11-16 12:59:28 -0300

    Config date

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -18,19 +18,23 @@ export default () => {
                 <FormStyle>
                     <TitleStyle fontSize='24'>Criar usuário</TitleStyle>
                     <SpaceStyle />
-                    
-                    <SpaceStyle padding='5' />
+
                     <TextFieldStyle label='Nome' value={name} onChange={(e) => setName(e.target.value)} value={name} />
+                    <SpaceStyle />
                     
-                    <SpaceStyle padding='5' />
                     <TextFieldStyle label='Vaga' value={stack} onChange={(e) => setStack(e.target.value)} value={stack} />
+                    <SpaceStyle />
                     
-                    <SpaceStyle padding='5' />
-                    <TextFieldStyle label='Data de Nascimento' value={birth} onChange={(e) => setBrith(e.target.value)} value={birth} />
+                    <TextFieldStyle label='Data de Nascimento' type="date" 
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    onChange={(e) => setBrith(e.target.value)} value={birth} />
+                    <SpaceStyle />
 
                     <TextFieldStyle label='Email' value={email} onChange={(e) => setEmail(e.target.value)} value={email} />
-
                     <SpaceStyle />
+                    
                     <ButtonStyle variant="contained" color="primary" width="280" onClick={handleUser}>Enviar</ButtonStyle>
                 </FormStyle>
             </PaperStyle>
