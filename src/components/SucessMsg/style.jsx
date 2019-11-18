19c9e16c6382d9d36fdfbd9602e794e1b95fedef
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-11-18 14:11:25 -0300

    Align message of sucess

--- src/components/SucessMsg/style.jsx
+++ src/components/SucessMsg/style.jsx
@@ -4,10 +4,12 @@ export const MessageStyle = styled.div`
     color: #43a047;
 
     h1 {
+        text-align: center;
         font-size: 36px;
     }
 
     h3 {
+        text-align: center;
         font-size: 14px;
     }
 `
